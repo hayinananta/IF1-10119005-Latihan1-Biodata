72b1fb4bdf6ebb470d66c1e54cb701b4762b3b1f
hayinanjay <72958200+hayinanjay@users.noreply.github.com>
2020-10-16 10:35:07 +0700

    Add files via upload

---
+++ src/pbo1/pkg10119005/latihan1/PBO110119005Latihan1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10119005.latihan1;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Program ini menampilkan jenis-jenis tipe data bilangan
+ *                     bulat
+ */
+public class PBO110119005Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Hayin Ananta");
+        System.out.println("ALAMAT\t\t: Jln.Hos Cokroaminoto No.15");
+        System.out.println("TEMPAT LAHIR\t: Jambi");
+        System.out.println("TANGGAL LAHIR\t: 03 Juni 20001");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Game");
+        System.out.println("MAKES\t\t: Soto");
+        System.out.println("MIKES\t\t: Es Teh");
+    }
+    
+}
